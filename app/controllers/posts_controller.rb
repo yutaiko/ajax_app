3e1e21bf103014ebdd376773929f6380ddac370f
yutaiko <yuta.ashikawa1984@gmail.com>
2020-07-27 01:03:45 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def new
   end
+
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
   def checked
     post = Post.find(params[:id])
     if post.checked then
@@ -17,6 +20,6 @@ class PostsController < ApplicationController
     end
 
     item = Post.find(params[:id])
-    render json: { post: item}
+    render json: { post: item }
   end
 end
