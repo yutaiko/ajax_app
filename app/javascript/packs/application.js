6fade8896ef60bc679bea0d84b2777576fe3238d
yutaiko <yuta.ashikawa1984@gmail.com>
2020-07-27 00:48:54 +0900

    Revert "修正"
    This reverts commit b1b9f79f76f2a3b49657b85d27e69c5f74aef973.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../checked")
-require("../memo")
 
 require("../checked")
 require("../memo")
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-<<<<<<< Updated upstream
   post 'posts' , to: 'posts#create'
-=======
-  post 'posts', to: 'posts#create'
->>>>>>> Stashed changes
   get 'posts/:id', to: 'posts#checked'
 end
